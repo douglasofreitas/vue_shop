970441b33cf7a1e677ef4f4a3932ffde5f6e7b4a
Douglas Freitas <douglasfreitas@natura.net>
2018-01-21 09:50:40 -0200

    add Vuex in Vue app

--- scotch-store/src/main.js
+++ scotch-store/src/main.js
@@ -1,9 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 import App from './App';
 import router from './router';
-import VeeValidate from 'vee-validate';
+import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(VeeValidate);
@@ -11,6 +12,7 @@ Vue.use(VeeValidate);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App },
